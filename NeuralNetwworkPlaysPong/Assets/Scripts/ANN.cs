e66942eae741851c696118738f4b6e3fbe9cecc0
muriloms <ms7murilo@gmail.com>
2020-12-10 18:28:37 -0300

    first commit

---
+++ NeuralNetwworkPlaysPong/Assets/Scripts/ANN.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ANN 
+{
+    public int numInputs;
+    public int numOutputs;
+    public int numHidden;
+    public int numNPerHidden;
+    public double alpha;
+    
+    private List<Layer> layers = new List<Layer>();
+
+    public ANN(int nI, int nO, int nH, int nPH, double a)
+    {
+        numInputs = nI;
+        numOutputs = nO;
+        numHidden = nH;
+        numNPerHidden = nPH;
+        alpha = a;
+
+        if (numHidden > 0)
+        {
+            layers.Add(new Layer(numNPerHidden, numInputs));
+
+            for (int i = 0; i < numHidden - 1; i++)
+            {
+                layers.Add(new Layer(numNPerHidden, numNPerHidden));
+            }
+            layers.Add(new Layer(numOutputs, numNPerHidden));
+        }
+        else
+        {
+            layers.Add(new Layer(numOutputs, numInputs));
+        }
+    }
+}
